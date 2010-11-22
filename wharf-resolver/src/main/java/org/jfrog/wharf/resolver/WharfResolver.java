70275ace41c03ec793d6dd7fbf8daf0e3b7c6792
Tomer Cohen <tomerc@jfrog.org>
2010-11-22 12:39:24 +0000

    compute checksum for module descriptor

--- wharf-resolver/src/main/java/org/jfrog/wharf/resolver/WharfResolver.java
+++ wharf-resolver/src/main/java/org/jfrog/wharf/resolver/WharfResolver.java
@@ -15,13 +15,18 @@
  */
 package org.jfrog.wharf.resolver;
 
+import org.apache.ivy.core.module.descriptor.Artifact;
 import org.apache.ivy.core.module.descriptor.DependencyDescriptor;
 import org.apache.ivy.core.resolve.ResolveData;
 import org.apache.ivy.core.resolve.ResolvedModuleRevision;
 import org.apache.ivy.plugins.resolver.IBiblioResolver;
+import org.apache.ivy.util.ChecksumHelper;
 import org.jfrog.wharf.ivy.cache.WharfCacheManager;
+import org.jfrog.wharf.ivy.model.ArtifactMetadata;
 import org.jfrog.wharf.ivy.model.ModuleRevisionMetadata;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -65,6 +70,10 @@ public class WharfResolver extends IBiblioResolver {
 
     private CacheTimeoutStrategy snapshotTimeout = DAILY;
 
+    {
+        setChecksums("md5, sha1");
+    }
+
     /**
      * Returns the timeout strategy for a Maven Snapshot in the cache
      */
@@ -99,9 +108,29 @@ public class WharfResolver extends IBiblioResolver {
             return null;
         }
         ModuleRevisionMetadata metadata = getCacheProperties(dd, moduleRevision);
-        Long lastResolvedTime = getLastResolvedTime(metadata);
-        updateCachePropertiesToCurrentTime(metadata);
         WharfCacheManager cacheManager = (WharfCacheManager) getRepositoryCacheManager();
+        Artifact artifact = moduleRevision.getDescriptor().getMetadataArtifact();
+        int id = cacheManager.getResolverHandler().getResolver(moduleRevision.getResolver()).getId();
+        artifact = ArtifactMetadata.fillResolverId(artifact, id);
+        ArtifactMetadata artMd = cacheManager.getMetadataHandler().getArtifactMetadata(artifact);
+        String[] algorithms = getChecksumAlgorithms();
+        for (String algorithm : algorithms) {
+            File localFile = moduleRevision.getReport().getLocalFile();
+            try {
+                String computedChecksum = ChecksumHelper.computeAsString(localFile, algorithm);
+                if ("sha1".equals(algorithm)) {
+                    artMd.sha1 = computedChecksum;
+                } else if ("md5".equals(algorithm)) {
+                    artMd.md5 = computedChecksum;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        metadata.artifactMetadata.remove(artMd);
+        metadata.artifactMetadata.add(artMd);
+        updateCachePropertiesToCurrentTime(metadata);
+        Long lastResolvedTime = getLastResolvedTime(metadata);
         cacheManager.getMetadataHandler().saveModuleRevisionMetadata(moduleRevision.getId(), metadata);
         if (snapshotTimeout.isCacheTimedOut(lastResolvedTime)) {
             setChangingPattern(".*-SNAPSHOT");
